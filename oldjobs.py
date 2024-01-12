7defd609306e0229ba1438da618143e7491fee03
zubairB373 <77339529+zubairB373@users.noreply.github.com>
2024-01-12 14:19:38 +0530

    Update oldjobs.py

--- oldjobs.py
+++ oldjobs.py
@@ -12,10 +12,21 @@ JENKINS_USERNAME = "zubair.bhat"
 JENKINS_PASSWORD = "Python@12345"
 
 
-class DevOpsJenkins:
-    def __init__(self):
-        self.jenkins_server = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
-        user = self.jenkins_server.get_whoami()
-        version = self.jenkins_server.get_version()
-        print ("Jenkins Version: {}".format(version))
-        print ("Jenkins User: {}".format(user['id']))
+#Jenkins connection
+
+j = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
+
+count = 0
+six_months_ago = (time.time() - 15768000)
+for jobname in j.jobnames:
+	job = j.job(jobname)
+	try:
+		if j.job_exists(jobname):
+			if job.last_build.info['timestamp']:
+				# Jenkins appears to add microseconds to the timestamp.
+				epoch_time = job.last_build.info['timestamp'] / 1000
+				if epoch_time < six_months_ago:
+					count += 1
+					print '{}: {}'.format(count, jobname)
+	except:
+		continue
