7e274d97ae6fe96cf04c88a93af32e89c461e46f
zubairB373 <77339529+zubairB373@users.noreply.github.com>
2024-01-12 15:10:57 +0530

    Update oldjobs.py

--- oldjobs.py
+++ oldjobs.py
@@ -1,25 +1,13 @@
 import jenkins
-# http://jenkins-webapi.readthedocs.org/en/latest/
-#from jira.client import JIRA
-import re
-import xml.etree.ElementTree as ET
-import sys
 import time
-import requests
+# It comes with pip module python-jenkins
+# use pip to install python-jenkins
 
-JENKINS_URL = "https://care-jenkins.qa.care.usw2.khorostech.com"
+# Jenkins Authentication URL
+JENKINS_URL = "https://care-jenkins.qa.care.usw2.khorostech.com/"
 JENKINS_USERNAME = "zubair.bhat"
 JENKINS_PASSWORD = "Python@12345"
-
-
-class DevOpsJenkins:
-    def __init__(self):
-        self.jenkins_server = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
-        user = self.jenkins_server.get_whoami()
-        version = self.jenkins_server.get_version()
-        print ("Jenkins Version: {}".format(version))
-        print ("Jenkins User: {}".format(user['id']))
-
-
-jenkins_obj = DevOpsJenkins()
-output = jenkins_obj._init_()
+server = jenkins.Jenkins('JENKINS_URL', username='JENKINS_USERNAME', password='JENKINS_PASSWORD')
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
