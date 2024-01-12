80dc9531fdf60afe4c6086c6c2836d41b6aab0fc
zubairB373 <77339529+zubairB373@users.noreply.github.com>
2024-01-12 15:16:16 +0530

    Update oldjobs.py

--- oldjobs.py
+++ oldjobs.py
@@ -7,7 +7,7 @@ import time
 JENKINS_URL = "https://care-jenkins.qa.care.usw2.khorostech.com/"
 JENKINS_USERNAME = "zubair.bhat"
 JENKINS_PASSWORD = "Python@12345"
-server = jenkins.Jenkins('JENKINS_URL', username='JENKINS_USERNAME', password='JENKINS_PASSWORD')
+server = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
