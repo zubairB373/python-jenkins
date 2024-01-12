12703f8d8db0c0ed9f6c9438fa4f816d781940e1
zubairB373 <77339529+zubairB373@users.noreply.github.com>
2024-01-12 17:24:01 +0530

    Update oldjobs.py

--- oldjobs.py
+++ oldjobs.py
@@ -10,11 +10,15 @@ JENKINS_PASSWORD = "Python@12345"
 
 class DevOpsJenkins:
     def __init__(self):
-        self.jenkins_server = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
-        user = self.jenkins_server.get_whoami()
-        version = self.jenkins_server.get_version()
-        print ("Jenkins Version: {}".format(version))
-        print ("Jenkins User: {}".format(user['id']))
+        try:
+            self.jenkins_server = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
+            user = self.jenkins_server.get_whoami()
+            version = self.jenkins_server.get_version()
+            print ("Jenkins Version: {}".format(version))
+            
+            print ("Jenkins User: {}".format(user['id']))
+        except JenkinsException as e:
+        print(f"Error: {e}")
 
     def build_job(self, name, token):
         next_build_number = self.jenkins_server.get_job_info(name)['nextBuildNumber']
